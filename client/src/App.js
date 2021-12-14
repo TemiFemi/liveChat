42da7c64f8fea4af11818c809106a23065ddd94a
Temitayo <oluwatemi@gmail.com>
2021-12-13 23:09:37 -0800

    added function that only displays chat when someone has joined the chat.

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
@@ -17,19 +18,23 @@ function App() {
   }
   return (
     <div className="App">
-      <div className="joinChatContainer">
-        <h3> Join A Chat</h3>
-        <input type="text" placeholder="John..." onChange={(event) => {
-          setUsername(event.target.value)
-        }} />
-        <input type="text" placeholder="Room ID..." onChange={(event) => {
-          setRoom(event.target.value)
-        }} />
-        <button onClick={joinRoom} > Join A Room </button>
-      </div>
+      {!showChat ? (
+        < div className="joinChatContainer">
+          <h3> Join A Chat</h3>
+          <input type="text" placeholder="John..." onChange={(event) => {
+            setUsername(event.target.value)
+          }} />
+          <input type="text" placeholder="Room ID..." onChange={(event) => {
+            setRoom(event.target.value)
+          }} />
+          <button onClick={joinRoom} > Join A Room </button>
+        </div>
+      )
+        : (
 
-      <Chat socket={socket} username={username} room={room} />
-    </div>
+          <Chat socket={socket} username={username} room={room} />
+        )}
+    </div >
   );
 }
 
